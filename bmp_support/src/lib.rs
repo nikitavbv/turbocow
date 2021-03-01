0dd5ecebf203d68ff375ff37bd5ce2b8cbfc0029
Nikita Volobuev <nikitavbv@gmail.com>
2021-03-02 00:11:42 +0200

    feat(core): load bmp support as plugin

--- bmp_support/src/lib.rs
+++ bmp_support/src/lib.rs
@@ -1,2 +1,17 @@
+use core::models::{ImageReader, ImageWriter};
+
+use reader::BMPReader;
+use writer::BMPWriter;
+
 pub mod reader;
 pub mod writer;
+
+#[no_mangle]
+pub fn init_reader() -> Box<dyn ImageReader> {
+    Box::new(BMPReader {})
+}
+
+#[no_mangle]
+pub fn init_writer() -> Box<dyn ImageWriter> {
+    Box::new(BMPWriter {})
+}
--- core/src/main.rs
+++ core/src/main.rs
@@ -1,4 +1,24 @@
+extern crate libloading;
+
+use libloading::{Library, Symbol};
+
+use models::ImageReader;
+
 pub mod models;
+
+type ReaderProvider = unsafe fn() -> Box<dyn ImageReader>;
+
 fn main() {
     println!("Hello, world!");
+
+    unsafe { 
+        let lib = Library::new("./plugins/libbmp_support.so")
+            .expect("failed to load bmp plugin");
+        let func: Symbol<ReaderProvider> = lib.get(b"init_reader").unwrap();
+    
+        let reader = func();
+        reader.read(&[]);
+
+        println!("done");
+    }
 }
