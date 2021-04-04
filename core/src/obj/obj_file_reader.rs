4cc4eee5d9e93fd13e76065688b582a24b8dd533
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2021-04-05 00:54:07 +0300

    feat(obj): add error handling

--- core/src/obj/obj_file_reader.rs
+++ core/src/obj/obj_file_reader.rs
@@ -2,6 +2,13 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 use crate::geometry::models::{Vector3, Vertex, Polygon};
 use regex::Regex;
+use custom_error::custom_error;
+
+custom_error! {pub ObjFileError
+    VertexError {description: String} = "Failed to parse vertex: {description}",
+    VertexNormalError {description: String} = "Failed to parse vertex normal: {description}",
+    FaceError {description: String} = "Failed to parse face: {description}",
+}
 
 #[derive(Debug)]
 pub struct ObjFile {
@@ -19,7 +26,7 @@ impl ObjFile {
         }
     }
 
-    pub fn load(&mut self, filename: &str) -> Result<(), ()> {
+    pub fn load(&mut self, filename: &str) -> Result<(), ObjFileError> {
         let vertex_normal = "vn";
         let vertex = "v";
         let comment = "#";
@@ -32,11 +39,11 @@ impl ObjFile {
             if line_data.len() == 0 || line_data.starts_with(comment) {
                 continue;
             } else if line_data.starts_with(vertex_normal) {
-                self.parse_vertex_normal(line_data);
+                self.parse_vertex_normal(line_data)?;
             } else if line_data.starts_with(vertex) {
-                self.parse_vertex(line_data);
+                self.parse_vertex(line_data)?;
             } else if line_data.starts_with(face) {
-                self.parse_face(line_data);
+                self.parse_face(line_data)?;
             } else {
                 panic!("Unable to parse line: {}", line_data);
             }
@@ -44,38 +51,91 @@ impl ObjFile {
         Result::Ok(())
     }
 
-    fn parse_vertex_normal(&mut self, line: String) {
+    fn parse_vertex_normal(&mut self, line: String) -> Result<(), ObjFileError> {
         let line = Regex::new(r"\\s+").unwrap().replace_all(line.as_str(), " ");
         let mut values = line.split(" ");
         values.next(); // skip 'vn'
-        let x = values.next().unwrap().parse::<f64>().unwrap();
-        let y = values.next().unwrap().parse::<f64>().unwrap();
-        let z = values.next().unwrap().parse::<f64>().unwrap();
+        let x = values.next();
+        if x.is_none() {
+            return Result::Err(ObjFileError::VertexNormalError {
+                description: format!("Unable to parse first coordinate: {}", &line)
+            });
+        }
+        let x = x.unwrap().parse::<f64>().map_err(|err| ObjFileError::VertexNormalError {
+            description: format!("Unable to parse first coordinate: {}. Cause: {:?}", &line, err)
+        })?;
+        let y = values.next();
+        if y.is_none() {
+            return Result::Err(ObjFileError::VertexNormalError {
+                description: format!("Unable to parse second coordinate: {}", &line)
+            });
+        }
+        let y = y.unwrap().parse::<f64>().map_err(|err| ObjFileError::VertexNormalError {
+            description: format!("Unable to parse second coordinate: {}. Cause: {:?}", &line, err)
+        })?;
+        let z = values.next();
+        if z.is_none() {
+            return Result::Err(ObjFileError::VertexNormalError {
+                description: format!("Unable to parse third coordinate: {}", &line)
+            });
+        }
+        let z = z.unwrap().parse::<f64>().map_err(|err| ObjFileError::VertexNormalError {
+            description: format!("Unable to parse third coordinate: {}. Cause: {:?}", &line, err)
+        })?;
         self.vertices_normals.push(Vector3::new(x, y, z));
+        Result::Ok(())
     }
 
-    fn parse_vertex(&mut self, line: String) {
+    fn parse_vertex(&mut self, line: String) -> Result<(), ObjFileError> {
         let line = Regex::new(r"\\s+").unwrap().replace_all(line.as_str(), " ");
         let mut values = line.split(" ");
         values.next(); // skip 'v'
-        let x = values.next().unwrap().parse::<f64>().unwrap();
-        let y = values.next().unwrap().parse::<f64>().unwrap();
-        let z = values.next().unwrap().parse::<f64>().unwrap();
+        let x = values.next();
+        if x.is_none() {
+            return Result::Err(ObjFileError::VertexNormalError {
+                description: format!("Unable to parse first coordinate: {}", &line)
+            });
+        }
+        let x = x.unwrap().parse::<f64>().map_err(|err| ObjFileError::VertexNormalError {
+            description: format!("Unable to parse first coordinate: {}. Cause: {:?}", &line, err)
+        })?;
+        let y = values.next();
+        if y.is_none() {
+            return Result::Err(ObjFileError::VertexNormalError {
+                description: format!("Unable to parse second coordinate: {}", &line)
+            });
+        }
+        let y = y.unwrap().parse::<f64>().map_err(|err| ObjFileError::VertexNormalError {
+            description: format!("Unable to parse second coordinate: {}. Cause: {:?}", &line, err)
+        })?;
+        let z = values.next();
+        if z.is_none() {
+            return Result::Err(ObjFileError::VertexNormalError {
+                description: format!("Unable to parse third coordinate: {}", &line)
+            });
+        }
+        let z = z.unwrap().parse::<f64>().map_err(|err| ObjFileError::VertexNormalError {
+            description: format!("Unable to parse third coordinate: {}. Cause: {:?}", &line, err)
+        })?;
         self.vertices.push(Vector3::new(x, y, z));
+        Result::Ok(())
     }
 
-    fn parse_face(&mut self, line: String) {
+    fn parse_face(&mut self, line: String) -> Result<(), ObjFileError> {
         let line = Regex::new(r"\\s+").unwrap().replace_all(line.as_str(), " ");
         let mut values = line.split(" ");
         values.next(); // skip 'f'
         let mut vertices = Vec::new();
         for _ in 0..3 {
             let mut value = values.next().unwrap().split("/");
-            let vertex_number = value.next().unwrap().parse::<usize>().unwrap();
+            let vertex_number = value.next().unwrap().parse::<usize>().map_err(|err| ObjFileError::FaceError {
+                description: format!("Unable to parse vertex number for face: {}. Cause: {:?}", line, err)
+            })?;
             if let Some(_) = value.next() {
                 if let Some(vn) = value.next() {
-                    println!("Why I'm here");
-                    let vertex_normal = vn.parse::<usize>().unwrap();
+                    let vertex_normal = vn.parse::<usize>().map_err(|err| ObjFileError::FaceError {
+                        description: format!("Unable to parse vertex normal number for face: {}. Cause: {:?}", line, err)
+                    })?;
                     vertices.push(Vertex::new(self.vertices[vertex_number - 1], self.vertices_normals[vertex_normal - 1]));
                 } else {
                     vertices.push(Vertex::new(self.vertices[vertex_number - 1], Vector3::zero()));
@@ -85,6 +145,7 @@ impl ObjFile {
             }
         }
         self.polygons.push(Polygon::new(vertices));
+        Result::Ok(())
     }
 }
 
@@ -98,5 +159,17 @@ mod tests {
         assert_eq!(model.vertices.len(), 6);
         assert_eq!(model.vertices_normals.len(), 0);
         assert_eq!(model.polygons.len(), 4);
+    }
+
+    #[test]
+    fn test2() {
+        let mut model = ObjFile::new();
+        let res = model.load("./assets/broken.obj");
+        match res {
+            Ok(_) => panic!("Test should fail due to bad input file!"),
+            Err(err) => {
+                assert_eq!(format!("{:?}", err), "VertexNormalError { description: \"Unable to parse first coordinate: v 2.292fw449 -0.871852 -0.882400. Cause: ParseFloatError { kind: Invalid }\" }");
+            },
+        };
     }
 }
